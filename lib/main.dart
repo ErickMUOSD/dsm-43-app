8706604644790577da1f6d67f7ebb738594c5982
Erick Sanabria <martinoli944@gmail.com>
2021-10-07 15:48:38 -0500

    refactored code

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 
-void main() => runApp(Button());
+import 'src/app.dart';
 
-class Button extends StatelessWidget {
-  const Button({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      body: Center(
-        child: Text('Centered'),
-      ),
-    ));
-  }
-}
+void main() => runApp(const MyApp());
