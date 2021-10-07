55fb259308fe1f068d5d665289fe517be730a707
Erick Sanabria <martinoli944@gmail.com>
2021-10-07 16:10:20 -0500

    row columns, textstyle

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -7,10 +7,58 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Title'),
+        title: const Text('Title'),
       ),
-      body: Center(
-        child: Text('Click here'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.amber[200],
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.amber[300],
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.amber[400],
+              ),
+            ],
+          ),
+          const Text(
+            'Numero de click',
+            style: TextStyle(fontSize: 20),
+          ),
+          const Text(
+            '0',
+            style: TextStyle(fontSize: 20, color: Colors.red),
+          ),
+          Row(
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.amber[200],
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.amber[300],
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.amber[400],
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
