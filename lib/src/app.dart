10448a73335b43be878d240bead4fa83dfa2660a
Erick Sanabria <martinoli944@gmail.com>
2021-10-07 15:56:51 -0500

    scaffold appbar

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      body: Center(
-        child: Text('Factored'),
-      ),
-    ));
+    return const MaterialApp(home: HomePage());
   }
 }
