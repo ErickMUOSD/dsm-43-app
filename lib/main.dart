0fdb7a22bcf67f981c53e72e0c8870f6ccf1201d
Erick Sanabria <martinoli944@gmail.com>
2021-10-07 15:43:02 -0500

    my first widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,18 @@
-import 'dart:developer';
-
+import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(Button());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Button extends StatelessWidget {
+  const Button({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
+        home: Scaffold(
+      body: Center(
+        child: Text('Centered'),
       ),
-      debugShowCheckedModeBanner: false,
-      home: const MyHomePage(
-        title: 'Flutter app',
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void onTap() {
-    setState(() {
-      _counter++;
-      print(_counter);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              GestureDetector(
-                onTap: onTap,
-                child: Container(
-                  padding: const EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Colors.green[400],
-                  ),
-                  child: const Text(
-                    'Click here',
-                    style: TextStyle(fontSize: 25.0, color: Colors.white),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        )
-        // floatingActionButton: FloatingActionButton(
-        //   onPressed: _incrementCounter,
-        //   tooltip: 'Increment',
-        //   child: const Icon(Icons.add),
-        // ), // This trailing comma makes auto-formatting nicer for build methods.
-        );
+    ));
   }
 }
