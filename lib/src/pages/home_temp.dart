e34bb9f504ebd071081c78623020a28976825e7e
Erick Sanabria <martinoli944@gmail.com>
2021-10-16 19:37:58 -0500

    list view builder

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -10,6 +10,20 @@ class HomePageTemp extends StatefulWidget {
 class _HomePageTempState extends State<HomePageTemp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes'),
+      ),
+      body: ListView.builder(
+        itemCount: 200,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text('Helloo $index'),
+            leading: const Icon(Icons.ac_unit_sharp),
+            subtitle: const Text('Subb'),
+          );
+        },
+      ),
+    );
   }
 }
