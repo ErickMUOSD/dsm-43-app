cd5358c1467ba059c8fc0470f4cc654b82ec919e
Erick Sanabria <martinoli944@gmail.com>
2021-10-16 19:50:22 -0500

    list title en metodo

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,29 +1,37 @@
 import 'package:flutter/material.dart';
 
-class HomePageTemp extends StatefulWidget {
+class HomePageTemp extends StatelessWidget {
   HomePageTemp({Key? key}) : super(key: key);
 
-  @override
-  _HomePageTempState createState() => _HomePageTempState();
-}
-
-class _HomePageTempState extends State<HomePageTemp> {
+  final options = ['TileOne', 'TileTWo', 'TileThree', 'TileFour'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Componentes'),
+        title: const Text('Componentes'),
       ),
       body: ListView.builder(
-        itemCount: 200,
+        itemCount: options.length,
         itemBuilder: (BuildContext context, int index) {
           return ListTile(
-            title: Text('Helloo $index'),
+            title: Text('Value ${options[index]} on index: $index'),
             leading: const Icon(Icons.ac_unit_sharp),
             subtitle: const Text('Subb'),
           );
         },
       ),
     );
+  }
+
+  List<Widget> _listTile() {
+    List<ListTile> list = [];
+
+    for (var item in options) {
+      final widgetTile = ListTile(
+        title: Text(item),
+      );
+      list.add(widgetTile);
+    }
+    return list;
   }
 }
