3eeadc0596d504ac484f84223d2faa74a4ee8ba5
Erick Sanabria <martinoli944@gmail.com>
2021-10-17 20:47:15 -0500

    circle avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -8,6 +8,27 @@ class Avatar_Page extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: [
+          CircleAvatar(
+            child: Image(
+              image: NetworkImage(
+                  'https://pbs.twimg.com/media/FBwryNfXMBAq7gm?format=jpg&name=900x900'),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(right: 10.0),
+            child: const CircleAvatar(
+              child: Text('AYO'),
+              backgroundColor: Colors.white,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+              'https://pbs.twimg.com/media/FBwryNfXMBAq7gm?format=jpg&name=900x900'),
+        ),
       ),
     );
   }
