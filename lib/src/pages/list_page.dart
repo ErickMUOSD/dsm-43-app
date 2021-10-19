ddb53a5514c184ef85f1c9621b623e50d95163be
Erick Sanabria <martinoli944@gmail.com>
2021-10-18 21:39:24 -0500

    circular progress bar

--- lib/src/pages/list_page.dart
+++ lib/src/pages/list_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ListPage extends StatefulWidget {
@@ -11,28 +13,40 @@ class _ListPageState extends State<ListPage> {
   List<int> _list = [];
   int _lastItem = 0;
   final ScrollController _scrollController = ScrollController();
+  bool _isLoading = false;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _addList();
+    _addMoreItems();
 
     _scrollController.addListener(() {
       if (_scrollController.position.pixels ==
           _scrollController.position.maxScrollExtent) {
-        _addList();
+        fetchData();
       }
     });
   }
 
   @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _scrollController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('asdasd'),
-      ),
-      body: _listWidget(),
-    );
+        appBar: AppBar(
+          title: const Text('lists'),
+        ),
+        body: Stack(
+          children: [
+            _listWidget(),
+            _wdigetLoading(),
+          ],
+        ));
   }
 
   Widget _listWidget() {
@@ -47,12 +61,39 @@ class _ListPageState extends State<ListPage> {
     );
   }
 
-  void _addList() {
+  void _addMoreItems() {
     setState(() {
       for (var i = 0; i < 10; i++) {
         _lastItem += i;
         _list.add(_lastItem);
       }
     });
+  }
+
+  Future<void> fetchData() async {
+    _isLoading = true;
+    setState(() {});
+    final duration = Duration(seconds: 2);
+    Timer(duration, requestHTTP);
+  }
+
+  void requestHTTP() {
+    _isLoading = false;
+    _addMoreItems();
+  }
+
+  Widget _wdigetLoading() {
+    if (_isLoading)
+      return Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            CircularProgressIndicator(),
+          ],
+        ),
+      );
+    else
+      return Container();
   }
 }
