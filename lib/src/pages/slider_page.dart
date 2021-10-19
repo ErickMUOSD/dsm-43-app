b968b2acb695b143a5017f5ceed08c742b9af4b2
Erick Sanabria <martinoli944@gmail.com>
2021-10-18 20:01:23 -0500

    switch, checkbox disable enable

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -8,20 +8,21 @@ class SliderPage extends StatefulWidget {
 }
 
 class _SliderPageState extends State<SliderPage> {
-  double _sliderValue = 200.0, _width = 100, _height = 150;
+  double _sliderValue = 200.0, _width = 100, _height = 100;
+  bool _enableCheck = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('SLiders'),
       ),
-      body: Container(
-        child: Column(
-          children: [
-            _slider(),
-            _image(),
-          ],
-        ),
+      body: Column(
+        children: [
+          _slider(),
+          _checkBox(),
+          _switchTitle(),
+          _image(),
+        ],
       ),
     );
   }
@@ -32,20 +33,44 @@ class _SliderPageState extends State<SliderPage> {
         min: 200,
         max: 500,
         label: 'SLider',
-        onChanged: (value) {
-          setState(() {
-            _sliderValue = value;
-          });
-        });
+        onChanged: (_enableCheck)
+            ? (value) {
+                setState(() {
+                  _sliderValue = value;
+                });
+              }
+            : null);
   }
 
   Widget _image() {
     return Image(
       width: _sliderValue,
-      height: 300,
+      height: 150,
       fit: BoxFit.contain,
-      image: NetworkImage(
+      image: const NetworkImage(
           'https://pbs.twimg.com/media/FBwryNfXMBAq7gm?format=jpg&name=900x900'),
     );
+  }
+
+  Widget _checkBox() {
+    return CheckboxListTile(
+        title: const Text('Enable resize image'),
+        value: _enableCheck,
+        onChanged: (value) {
+          setState(() {
+            _enableCheck = value!;
+          });
+        });
+  }
+
+  Widget _switchTitle() {
+    return SwitchListTile(
+        title: const Text('Enable resize image'),
+        value: _enableCheck,
+        onChanged: (value) {
+          setState(() {
+            _enableCheck = value;
+          });
+        });
   }
 }
