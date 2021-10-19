336b31ea343aff43c5ebb34036a18a19f03e8759
Erick Sanabria <martinoli944@gmail.com>
2021-10-18 21:01:19 -0500

    infinite images

--- lib/src/pages/list_page.dart
+++ lib/src/pages/list_page.dart
@@ -8,13 +8,23 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
-  List<int> _list = [
-    1,
-    2,
-    3,
-    4,
-    5,
-  ];
+  List<int> _list = [];
+  int _lastItem = 0;
+  final ScrollController _scrollController = ScrollController();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _addList();
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _addList();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,12 +37,22 @@ class _ListPageState extends State<ListPage> {
 
   Widget _listWidget() {
     return ListView.builder(
+      controller: _scrollController,
       itemBuilder: (BuildContext context, index) {
         final image = _list[index];
         return Image(
-            image: NetworkImage('https://picsum.photos/id/${image}/100/150'));
+            image: NetworkImage('https://picsum.photos/id/${image}/500/300'));
       },
       itemCount: _list.length,
     );
+  }
+
+  void _addList() {
+    setState(() {
+      for (var i = 0; i < 10; i++) {
+        _lastItem += i;
+        _list.add(_lastItem);
+      }
+    });
   }
 }
